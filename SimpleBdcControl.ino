c967bd6b1221984ba9227c735bb90cca02537446
mzninja <michaelzeyer@t-online.de>
2025-09-22 20:37:52 +0200

    Switches linked to powerstages

--- SimpleBdcControl.ino
+++ SimpleBdcControl.ino
@@ -1,4 +1,5 @@
 #include "PwrStgCtl_Ifx9201.h"
+#include "SimpleBdcControl.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,10 +7,35 @@ void setup() {
   pinMode(3, INPUT);
   pinMode(12, INPUT);
   pinMode(13, INPUT);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(digitalRead(MechSwtch1_Pin))
+  {
+    PwrStg1.setPwrStg(PWRSTG_ACTIVE, 1, 50);  
+  }
+  else if(digitalRead(MechSwtch3_Pin))
+  {
+    PwrStg1.setPwrStg(PWRSTG_ACTIVE, 0, 50);  
+  }
+  else
+  {
+    PwrStg1.setPwrStg(PWRSTG_INACTIVE, 1, 0);  
+  }
+  
+  if(digitalRead(MechSwtch2_Pin))
+  {
+    PwrStg2.setPwrStg(PWRSTG_ACTIVE, 1, 50);  
+  }
+  else if(digitalRead(MechSwtch4_Pin))
+  {
+    PwrStg2.setPwrStg(PWRSTG_ACTIVE, 0, 50);  
+  }
+  else
+  {
+    PwrStg2.setPwrStg(PWRSTG_INACTIVE, 1, 0);  
+  }
+  delay(100);
 
 }
